861457f362668f518c05295eab30389903c36495
Jordan-Conway <D00244954@student.dkit.ie>
2023-02-17 09:04:25 +0000

    Item no longer has price value

--- server/models/items.js
+++ server/models/items.js
@@ -5,7 +5,6 @@ const mongoose = require(`mongoose`)
 let itemSchema = new mongoose.Schema(
     {
         itemId:{type: Number},
-        itemName:{type: String},
         tags:[{
             type:String
         }]
