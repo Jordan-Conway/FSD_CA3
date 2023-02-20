1bd2082c13a788111b7e51659eca3abcea753856
Jordan-Conway <D00244954@student.dkit.ie>
2023-02-20 11:33:07 +0000

    Removed validation of images from schema

--- server/models/listings.js
+++ server/models/listings.js
@@ -8,23 +8,8 @@ let listingSchema = new mongoose.Schema(
         quantity:{type:Number, match:/^\d+$/},
         itemImages:[
             {type:String, default:""}
-        ],
-        validateImages: [validNumImages, 'Only 10 images allowed'] 
+        ]
     }
 )
-
-//Solution to validate number of images taken from https://stackoverflow.com/a/59480100
-const validNumImages = (val) => {
-    let validNum = true;
-
-    val.forEach(element => {
-        if(element.length > 10){
-            validNum = false;
-            return;
-        }
-    });
-
-    return validNum;
-}
 
 module.exports = mongoose.model(`listings`, listingSchema)
