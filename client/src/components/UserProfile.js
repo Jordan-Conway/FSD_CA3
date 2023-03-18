ec499f174947b2f0139fa3d06a516e187eda3393
Jordan-Conway <D00244954@student.dkit.ie>
2023-03-18 16:23:42 +0000

    Removed BuyAgain

--- client/src/components/UserProfile.js
+++ client/src/components/UserProfile.js
@@ -11,7 +11,6 @@ export default class UserProfile extends Component
             <div className="userProfile">
                 <UserDetails/>
                 <UserPurchases/>
-                <BuyAgain/>
             </div>
         )
     }
