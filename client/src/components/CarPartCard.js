c26d162ce4d935f50df170986b444f1763c48360
Jordan-Conway <D00244954@student.dkit.ie>
2023-03-19 18:24:52 +0000

    Cleaned up warnings

--- client/src/components/CarPartCard.js
+++ client/src/components/CarPartCard.js
@@ -1,10 +1,4 @@
 import React, { Component } from "react"
-import { Link } from "react-router-dom"
-
-import CarPartIMG from "./CarPartIMG"
-import axios from "axios"
-import { SERVER_HOST } from "../config/global_constants"
-
 
 export default class CarPartCard extends Component {
     constructor(props) {
