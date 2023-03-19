f1498e4957d06ac4fdc5f31f3dad86d394c8aa79
Jordan-Conway <D00244954@student.dkit.ie>
2023-03-19 19:07:40 +0000

    Add to Cart now redirects to login

--- client/src/components/CarPartCard.js
+++ client/src/components/CarPartCard.js
@@ -1,4 +1,6 @@
 import React, { Component } from "react"
+import { Link } from "react-router-dom"
+import { ACCESS_LEVEL_GUEST } from "../config/global_constants"
 
 export default class CarPartCard extends Component {
     constructor(props) {
@@ -52,7 +54,7 @@ export default class CarPartCard extends Component {
                 </div>
 
                 <div className="card_bottom">
-                    {this.state.addable ? <button className="add-to-cart-button" onClick={this.addToCart}>Add to Cart</button> : null}
+                    {localStorage.accessLevl > ACCESS_LEVEL_GUEST ? <button className="add-to-cart-button" onClick={this.addToCart}>Add to Cart</button> : <button className="add-to-cart-button"><Link to={"/Login"}>Add to Cart</Link></button>}
                 </div>
             </div>
 
